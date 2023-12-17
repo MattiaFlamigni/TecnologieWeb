e13aa0e3d02c09abe09a8650ae2c80f51ab0c1eb
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2023-12-17 18:18:02 +0100

    fixed mistake in writing

--- esercizio_PHP/database.php
+++ esercizio_PHP/database.php
@@ -1,9 +1,13 @@
 <?php 
+
+/**
+ * Classe per la connessione al database
+ */
 class DatabaseHelper{
     private $db;
 
     public function __construct($servername, $username, $password, $dbname){
-        $this->db = new msqli($servername, $username, $password, $dbname);
+        $this->db = new mysqli($servername, $username, $password, $dbname);
 
         if($this->db->connect_error){
             die("COnnection failed: " . $this->db->connect_error);
